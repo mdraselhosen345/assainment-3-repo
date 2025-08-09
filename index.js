9d4812856228d9d538adb051582d3c28e1617d2b
md rasel islam <rashel099009@gamil.com>
2025-08-09 12:28:06 +0600

    added the index.js

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 //-----Problem 01---------
- var area = 800;
- var areaPlace = area / 2;
- console.log(areaPlace);
+//  var area = 800;
+//  var areaPlace = area / 2;
+//  console.log(areaPlace);
 
- var area = 100;
- console.log(area / 2);
+//  var area = 100;
+//  console.log(area / 2);
 
- var area = 15;
- console.log(area / 2);
+//  var area = 15;
+//  console.log(area / 2);
 
- var area = 2060;
- console.log(area / 2);
-
-
+//  var area = 2060;
+//  console.log(area / 2);
 
 
 
+
+//----Problem 02--------
+var money = 45000;
+  
+if(money >= 45000){
+    console.log('Laptop');
+}
+else if(money >= 10000){
+    console.log('Cycle');
+}
+else{
+    console.log('Choclet');
+}
        
 
